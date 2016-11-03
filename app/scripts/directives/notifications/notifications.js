ee49424fd677656112b810ef2c509f3ef481edbe
Diego Hernandez <drhernandez92@gmail.com>
2016-11-03 10:08:27 -0300

    ahora la tabla no tiene los datos hardcodeados

--- app/scripts/directives/notifications/notifications.js
+++ app/scripts/directives/notifications/notifications.js
@@ -12,7 +12,21 @@ angular.module('sbAdminApp')
         templateUrl:'scripts/directives/notifications/notifications.html',
         restrict: 'E',
         replace: true,
+				link: link
     	}
+
+			function link(scope, element, attrs) {
+				scope.pedidos = [
+					{id: 0, name: 'Pedido #1'},
+					{id: 1, name: 'Pedido #2'},
+					{id: 2, name: 'Pedido #3'},
+					{id: 3, name: 'Pedido #4'},
+					{id: 4, name: 'Pedido #5'},
+					{id: 5, name: 'Pedido #6'},
+					{id: 6, name: 'Pedido #7'},
+					{id: 7, name: 'Pedido #8'},
+					{id: 8, name: 'Pedido #9'},
+					{id: 9, name: 'Pedido #10'}
+				];
+			}
 	});
-
-
