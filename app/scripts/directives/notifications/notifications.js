7af83764037a11ca5dcf1f165cbc73721b76fea6
fcabanilla <federico.cabanilla@gmail.com>
2016-11-04 03:32:22 -0300

    modificaciones en el modal para editar comandas, falta conectar elementos, solo estan los clientes y mozos

--- app/scripts/directives/notifications/notifications.js
+++ app/scripts/directives/notifications/notifications.js
@@ -20,15 +20,15 @@ angular.module('sbAdminApp')
 				var index = -1;
 
 				scope.pedidos = [
-					{id: 0, name: 'Pedido #1', mozo: 'Juan Carlos'},
-					{id: 1, name: 'Pedido #2', pedido:'', direccion:'', menu:'', mozo:'fede', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 2, name: 'Pedido #3', pedido:'', direccion:'', menu:'', mozo:'mica', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 3, name: 'Pedido #4', pedido:'', direccion:'', menu:'', mozo:'diego', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 4, name: 'Pedido #5', pedido:'', direccion:'', menu:'', mozo:'riso', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 5, name: 'Pedido #6', pedido:'', direccion:'', menu:'', mozo:'miguel', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 6, name: 'Pedido #7', pedido:'', direccion:'', menu:'', mozo:'boludo', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 7, name: 'Pedido #8', pedido:'', direccion:'', menu:'', mozo:'gon', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
-					{id: 8, name: 'Pedido #9', pedido:'', direccion:'', menu:'', mozo:'tu Vieja', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 0, name: 'Pedido #1', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 1, name: 'Pedido #2', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 2, name: 'Pedido #3', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 3, name: 'Pedido #4', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 4, name: 'Pedido #5', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 5, name: 'Pedido #6', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 6, name: 'Pedido #7', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 7, name: 'Pedido #8', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
+					{id: 8, name: 'Pedido #9', pedido:'', direccion:'', menu:'', mozo:'', cliente:'', entregado:'', cuentaCorriente:'', pagado:''},
 				];
 
 				scope.mozos =[
@@ -37,6 +37,18 @@ angular.module('sbAdminApp')
 					{id: 2, name: 'mica'},
 					{id: 3, name: 'Magui'}
 				];
+
+				scope.clientes =[
+					{id: 0, name: 'Camila', direccion: '13'},
+					{id: 1, name: 'moni', direccion: '342'},
+					{id: 2, name: 'Laura', direccion: '45341'},
+					{id: 3, name: 'Jose', direccion: '123'},
+					{id: 4, name: 'Pablo', direccion: '464'},
+				];
+
+				scope.dateTimePicker = function(){
+
+				}
 
 				scope.setIndex = function (i) {
 					index = i;
